f1b1d03b10d818da90f601d59e4062c6b0c8059a
Hstbchg66 <140915128+Hstbchg66@users.noreply.github.com>
2023-08-22 00:10:00 +0300

    config.py
    Signed-off-by: Hstbchg66 <140915128+Hstbchg66@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    DATABASE = os.environ.get("DB_URI")
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
-    SUPPORT = os.environ.get("SUPPORT")
+    DATABASE = os.environ.get("mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority")
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6025090138").split())
+    SUPPORT = os.environ.get("BXB_KING")
